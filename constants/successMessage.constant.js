8456a9bdee7024e15d12cc58b8a80a9c3f10f6d0
oorangeed <pgh9311@gmail.com>
2023-11-23 16:08:26 +0900

    feat: SuccessMessages.constant.js 충돌 수정

--- constants/successMessage.constant.js
+++ constants/successMessage.constant.js
@@ -2,5 +2,11 @@ export const SuccessMessages = Object.freeze({
   SIGNUP_SUCCESS: "회원가입에 성공했습니다.",
   LOGIN_SUCCESS: "로그인에 성공했습니다.",
   LOGOUT_SUCCESS: "로그아웃 되었습니다.",
+
+  // 게시물
+  CONTENT_SUCCESS: "게시물이 작성되었습니다.",
+  TOKEN_REFRESH_SUCCESS: "토큰 갱신에 성공하였습니다.",
+
+  //회원 조회
   PROFILE_LODING_CUCCESS: "프로필을 성공적으로 확인하였습니다."
 });
