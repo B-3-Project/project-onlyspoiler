455b5f81b92fb60f9ceb0fc59cfb71004e0416f5
yujin <108935743+yujin0708@users.noreply.github.com>
2023-11-24 15:44:58 +0900

    #32 feat(auth): 네이버 로그인 기능 추가
    - 네이버 로그인 임시 버튼 추가
    - 소셜로그인 테이블 추가
    - 네이버 로그인 로직 추가
    - passport 라이브러리 추가

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import { SERVER_PORT } from "./constants/app.constant.js";
 import authRouter from "./routers/auth/auth.router.js";
 import contentsRouter from "./routers/contents/contents.router.js";
 import mailRouter from "./routers/auth/mail.router.js";
+import socialRouter from "./routers/auth/socialLogin.router.js";
 import { handleServerError } from "./middlewares/handleServerError.middleware.js";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("."));
 
-app.use("/api", [authRouter, contentsRouter, mailRouter]);
+app.use("/api", [authRouter, contentsRouter, mailRouter, socialRouter]);
 app.use(handleServerError);
 
 app.listen(SERVER_PORT, () => {
---
+++ routers/auth/socialLogin.router.js
@@ -0,0 +1,71 @@
+import express from "express";
+import db from "../../models/index.cjs";
+const { Users, SocialAccounts, RefreshTokens } = db;
+import "dotenv/config";
+import passport from "passport";
+import NaverStrategy from "passport-naver";
+import { generateToken } from "../../utils/token.js";
+
+const router = express.Router();
+
+passport.use(
+  new NaverStrategy(
+    {
+      clientID: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET,
+      callbackURL: "http://localhost:3006/api/naverCallback"
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const userEmail = profile.emails[0].value;
+
+      let user = await Users.findOne({ where: { email: userEmail } });
+
+      if (!user) {
+        user = await Users.create({
+          email: userEmail,
+          name: profile.displayName,
+          password: profile.displayName,
+          verified: true
+        });
+      } else {
+        await RefreshTokens.destroy({ where: { user_Id: user.id } });
+      }
+
+      let socialAccount = await SocialAccounts.findOne({
+        where: { email: userEmail }
+      });
+
+      if (!socialAccount) {
+        socialAccount = await SocialAccounts.create({
+          provider: "naver",
+          email: userEmail,
+          socialId: profile.id,
+          userId: user.id
+        });
+      }
+
+      const tokens = await generateToken(user.id);
+      done(null, { user, ...tokens });
+    }
+  )
+);
+
+router.get("/login-naver", passport.authenticate("naver", { session: false }));
+
+router.get(
+  "/naverCallback",
+  passport.authenticate("naver", {
+    session: false,
+    failureRedirect: "/fail.html"
+  }),
+  function (req, res) {
+    res.redirect(
+      "/login.html?accessToken=" +
+        req.user.accessToken +
+        "&refreshToken=" +
+        req.user.refreshToken
+    );
+  }
+);
+
+export default router;
