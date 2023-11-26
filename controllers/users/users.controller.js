154c112b2cc8ecd2a19d80a87d93d1c1066d3884
=lemon-table <=>
2023-11-27 03:26:37 +0900

    feat: 로그인 후 프로필화면 이동, 게시물 CRUD 구현

--- controllers/users/users.controller.js
+++ controllers/users/users.controller.js
@@ -12,93 +12,94 @@ import { createError } from "../../utils/errorResponse.js";
 
 // 회원 정보 조회
 export const getUserProfile = async (req, res, next) => {
-    try {
-        const userId = res.locals.decoded.userId;
-        const user = await Users.findByPk(userId);
+  try {
+    const userId = res.locals.decoded.userId;
+    const user = await Users.findByPk(userId);
 
-        if (!user) {
-            throw createError(
-                StatusCodes.BAD_REQUEST,
-                ErrorMessages.INVALID_LOGIN_INFO
-            );
-        }
-        res.status(StatusCodes.OK).json({
-            success: true,
-            message: SuccessMessages.PROFILE_LODING_SUCCESS,
-            data: {
-                id: userId,
-                username: user.name,
-                description: user.description || ""
-            }
-        });
-    } catch (err) {
-        next(err);
+    if (!user) {
+      throw createError(
+        StatusCodes.BAD_REQUEST,
+        ErrorMessages.INVALID_LOGIN_INFO
+      );
     }
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: SuccessMessages.PROFILE_LODING_SUCCESS,
+      data: {
+        id: userId,
+        username: user.name,
+        description: user.description || "",
+        email: user.email
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 회원 정보 수정
 export const putUserProfile = async (req, res, next) => {
-    try {
-        const userId = res.locals.decoded.userId;
-        const { name, email, description } = req.body;
+  try {
+    const userId = res.locals.decoded.userId;
+    const { name, email, description } = req.body;
 
-        const [updateProfile] = await Users.update(
-            { username: name, email, description },
-            { where: { id: userId } }
-        );
+    const [updateProfile] = await Users.update(
+      { name, email, description },
+      { where: { id: userId } }
+    );
 
-        if (updateProfile === 0) {
-            throw createError(
-                StatusCodes.NOT_FOUND,
-                ErrorMessages.PRORILE_NOT_MODIFY
-            );
-        }
-
-        const updateUser = await Users.findByPk(userId);
-
-        res.status(StatusCodes.OK).json({
-            success: true,
-            message: SuccessMessages.PROFILE_MODIFY_SUCCESS,
-            data: {
-                id: userId,
-                username: updateUser.name,
-                description: updateUser.description || ""
-            }
-        });
-    } catch (err) {
-        next(err);
+    if (updateProfile === 0) {
+      throw createError(
+        StatusCodes.NOT_FOUND,
+        ErrorMessages.PRORILE_NOT_MODIFY
+      );
     }
+
+    const updateUser = await Users.findByPk(userId);
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: SuccessMessages.PROFILE_MODIFY_SUCCESS,
+      data: {
+        id: userId,
+        username: updateUser.name,
+        description: updateUser.description || ""
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 비밀번호 수정
 export const changePassword = async (req, res, next) => {
-    try {
-        const userId = res.locals.decoded.userId;
-        const { currentPassword, newPassword } = req.body;
+  try {
+    const userId = res.locals.decoded.userId;
+    const { currentPassword, newPassword } = req.body;
 
-        const user = await Users.findByPk(userId);
-        if (!user) {
-            throw createError(
-                StatusCodes.BAD_REQUEST,
-                ErrorMessages.INVALID_USER_INFO
-            );
-        }
-
-        const validPassword = await bcrypt.compare(currentPassword, user.password);
-        if (!validPassword) {
-            throw createError(
-                StatusCodes.UNAUTHORIZED,
-                ErrorMessages.INVALID_CURRENT_PASSWORD
-            );
-        }
-
-        await user.update({ password: newPassword });
-
-        res.status(StatusCodes.OK).json({
-            success: true,
-            message: SuccessMessages.PASSWORD_MODIFY_SUCCESS
-        });
-    } catch (err) {
-        next(err);
+    const user = await Users.findByPk(userId);
+    if (!user) {
+      throw createError(
+        StatusCodes.BAD_REQUEST,
+        ErrorMessages.INVALID_USER_INFO
+      );
     }
+
+    const validPassword = await bcrypt.compare(currentPassword, user.password);
+    if (!validPassword) {
+      throw createError(
+        StatusCodes.UNAUTHORIZED,
+        ErrorMessages.INVALID_CURRENT_PASSWORD
+      );
+    }
+
+    await user.update({ password: newPassword });
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: SuccessMessages.PASSWORD_MODIFY_SUCCESS
+    });
+  } catch (err) {
+    next(err);
+  }
 };
